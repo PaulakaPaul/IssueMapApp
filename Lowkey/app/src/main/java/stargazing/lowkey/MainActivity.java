aaa99268e156831fa3478f9236356026f870cf4b
Iusztin Paul <p.e.iusztin@gmail.com>
2018-11-23 16:20:21 +0200

    first-ui-update

--- Lowkey/app/src/main/java/stargazing/lowkey/MainActivity.java
+++ Lowkey/app/src/main/java/stargazing/lowkey/MainActivity.java
@@ -1,30 +1,48 @@
 package stargazing.lowkey;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import stargazing.lowkey.main.fragments.IssuesFragment;
+import stargazing.lowkey.main.fragments.ProfileFragment;
+import stargazing.lowkey.main.fragments.StatisticFragment;
 
-    private TextView mTextMessage;
+public class MainActivity extends AppCompatActivity implements
+        ProfileFragment.OnFragmentInteractionListener,
+        IssuesFragment.OnFragmentInteractionListener,
+        StatisticFragment.OnFragmentInteractionListener {
+
+
+    final Fragment profileFragment = new ProfileFragment();
+    final Fragment issuesFragment = new IssuesFragment();
+    final Fragment statisticFragment = new StatisticFragment();
+    final FragmentManager fm = getSupportFragmentManager();
+    Fragment active = profileFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    fm.beginTransaction().hide(active).show(profileFragment).commit();
+                    active = profileFragment;
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    fm.beginTransaction().hide(active).show(issuesFragment).commit();
+                    active = issuesFragment;
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    fm.beginTransaction().hide(active).show(statisticFragment).commit();
+                    active = statisticFragment;
                     return true;
             }
             return false;
@@ -35,10 +53,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
+        fm.beginTransaction().add(R.id.main_container, issuesFragment, "2").hide(issuesFragment).commit();
+        fm.beginTransaction().add(R.id.main_container, statisticFragment, "3").hide(statisticFragment).commit();
+        fm.beginTransaction().add(R.id.main_container, profileFragment, "1").commit();
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 
 }
